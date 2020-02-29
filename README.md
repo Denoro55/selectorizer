b0c1a4de6dd723dae877fc0fedd45849ef532cd9
Denis Chertenko <denis.chertenko@mail.ru>
2020-02-29 18:25:09 +0300

    rewrite package logic

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
-Create simple wrapper for your selects and write your own styles for this.
+This package with native javascript will help you to create a simple wrapper for your selects and get useful methods.
+
+**Package requires babel**
+
+### implemented methods:
+- trigger (change, refresh)
+- on (selectorizer-change, selectorizer-before-open, selectorizer-open, selectorizer-before-close, selectorizer-close)
+
+### options:
+- nativeOnMobile
+- iconHtml
 
 ## How to use:
 
@@ -6,12 +16,45 @@ Create simple wrapper for your selects and write your own styles for this.
 import selectorize from 'selectorizer';
 import 'selectorizer/style.css';
 
-const select = document.querySelector('.select');
+const selects = document.querySelectorAll('select');
 
-selectorize(select);
+selectorize(selects, {
+    nativeOnMobile: true,
+    iconHtml: '<div class="my-icon"></div>'
+});
+
+selectorize(selects).trigger('change', 'value #7'); // changes value of all selects
+
+selectorize(selects).on('selectorizer-open', (select, value) => {
+    console.log('open!', select, value)
+});
+
+selectorize(selects).on('selectorizer-close', (select, value) => {
+    console.log('close!', select, value)
+});
+
+selectorize(selects).on('selectorizer-before-open', (select, value) => {
+    console.log('before open!', select, value)
+});
+
+selectorize(selects).on('selectorizer-before-close', (select, value) => {
+    console.log('before close!', select, value)
+});
+
+selectorize(selects).on('selectorizer-change', (select, value, prevValue) => { // handler
+    console.log('value changed ', select, value, prevValue);
+});
+
+const option = document.createElement('option');
+option.value = 'newValue';
+option.text = 'newValue';
+selects[0].append(option);
+
+setTimeout(() => {
+    selectorize(selects[0]).trigger('refresh'); // refresh list
+}, 3000);
 ```
-
 
 ----------------------------------------------------------
 
-Alpha version!
+Other methods and updates soon
--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+const pluginName = 'selectorizer';
+
+document.body.addEventListener('click', (e) => {
+    tryToClose(e);
+});
+
+const tryToClose = (e) => {
+    const selects = document.querySelectorAll('.selectorizer');
+    selects.forEach(el => {
+        if (!el.contains(e.target)) {
+            const select = el.querySelector('select');
+            select[pluginName].close();
+        }
+    })
+};
+
 const createElement = (tag, cls, text) => {
     const el = document.createElement(tag);
     el.className = cls;
@@ -5,203 +21,254 @@ const createElement = (tag, cls, text) => {
     return el;
 };
 
-const render = (select, list, text, state) => {
-    const nativeElement = select.querySelector('select');
-    nativeElement.selectedIndex = state.currentOptionIndex;
-    text.innerText = state.currentText;
-    if (state.active) {
-        list.style.display = 'block';
-        select.classList.add('opened');
-    } else {
-        list.style.display = 'none';
-        select.classList.remove('opened');
+const getSiblings = function (elem) {
+
+    // Setup siblings array and get the first sibling
+    var siblings = [];
+    var sibling = elem.parentNode.firstChild;
+
+    // Loop through each sibling and push to the array
+    while (sibling) {
+        if (sibling.nodeType === 1 && sibling !== elem) {
+            siblings.push(sibling);
+        }
+        sibling = sibling.nextSibling
     }
-    if (state.position === 'top') {
-        list.style.top = '100%';
-        list.style.bottom = 'auto';
-    } else {
-        list.style.top = 'auto';
-        list.style.bottom = '100%';
-    }
+
+    return siblings;
 };
 
-const change = (params) => {
-
-};
-
-const methods = {
-    'change': change
-};
-
-const selectorize = (elements, options) => {
-    if (elements === null) {
-        throw new TypeError('Argument is null');
+class Selectorizer {
+    constructor(select, params = {}) {
+        this.select = select;
+        this.state = {
+            opened: false,
+            position: 'top',
+            currentValue: ''
+        };
+        this.events = {};
+        this.params = params;
+        this.init();
     }
-    if (elements instanceof NodeList) {
-        elements.forEach(el => {
-            wrap(el, options);
-        })
-    } else {
-        wrap(elements, options);
-    }
-};
 
-const wrap = (select, options) => {
-    const state = {
-        active: false,
-        currentOptionIndex: 0,
-        currentText: '',
-        items: [],
-        callbacks: options.callbacks
-    };
+    init() {
+        const select = this.select;
 
-    const element = createElement('div', 'select', '');
+        if (this.isMobile()) {
+            select.classList.add('native');
+        }
 
-    const inner = createElement('div', 'select__inner', '');
-    element.appendChild(inner);
+        const element = createElement('div', pluginName, '');
 
-    const icon = createElement('div', 'select__icon', '');
-    inner.appendChild(icon);
+        const inner = createElement('div', `${pluginName}__inner`, '');
+        element.appendChild(inner);
 
-    const list = createElement('ul', 'select__list', '');
-    list.style.display = 'none';
+        const icon = createElement('div', `${pluginName}__icon`, '');
+        if (this.params.iconHtml) {
+            icon.innerHTML = this.params.iconHtml;
+        }
+        inner.appendChild(icon);
 
-    const currentOptions = select.querySelectorAll('option');
+        const currentOptions = this.select.querySelectorAll('option');
 
-    const text = createElement('div', 'select__text', currentOptions[0].innerText);
-    state.currentText = currentOptions[0].innerText;
-    inner.appendChild(text);
+        const text = createElement('div', `${pluginName}__text`, currentOptions[0].innerText);
+        inner.appendChild(text);
 
-    currentOptions.forEach((option, idx) => {
-        const optionText = option.text;
-        state.items.push({value: option.value, text: optionText});
-        const li = createElement('li', 'select__item', optionText);
-        li.addEventListener('click', (e) => {
-            e.stopPropagation();
-            state.active = false;
-            state.currentOptionIndex = idx;
-            state.currentText = state.items[idx].text;
-            if (state.callbacks.onchange) {
-                options.callbacks.onchange(select, state.items[idx].value);
+        this.state.currentValue = currentOptions[0].value;
+
+        const list = this.generateList();
+
+        element.appendChild(list);
+
+        element.addEventListener('click', (e) => {
+            if (this.isMobile()) {
+                return;
             }
-            render(element, list, text, state);
+            e.stopPropagation();
+            tryToClose(e);
+            this.toggle(this.state);
         });
 
-        list.appendChild(li);
-    });
+        select.after(element);
+        element.prepend(select);
 
-    element.appendChild(list);
+        select.addEventListener('change', (e) => {
+            this.change(e.target.value);
+        });
 
-    element.addEventListener('click', (e) => {
-        if (window.innerWidth < 768) return;
+        this.elements = {
+            selectorizer: element,
+            list,
+            text
+        }
+    }
+
+    isMobile() {
+        return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) && this.params.nativeOnMobile;
+    }
+
+    refresh() {
+        const newList = this.generateList();
+        this.elements.list.remove();
+        this.elements.selectorizer.append(newList);
+    }
+
+    updateValue(value) {
+        if (this.state.currentValue !== value) {
+            const prevValue = this.state.currentValue;
+            this.state.currentValue = value;
+            this.triggerEvent('selectorizer-change', prevValue);
+            return true;
+        }
+        return false;
+    }
+
+    change(value) {
+        if (this.updateValue(value)) {
+            this.render();
+        }
+    }
+
+    toggle(state) {
+        const selectorizer = this.elements.selectorizer;
+
         const position = window.pageYOffset;
         const h = window.innerHeight;
-        const elY = element.offsetTop;
+        const elY = selectorizer.offsetTop;
         const calc = (position + h) / 2;
-        if (calc < elY) {
-            state.position = 'bottom';
+
+        state.position = calc < elY ? 'bottom' : 'top';
+
+        if (state.opened) {
+            this.close();
         } else {
-            state.position = 'top';
+            this.open();
         }
-        state.active = !state.active;
-        render(element, list, text, state);
+    }
+
+    open() {
+        if (!this.state.opened) {
+            this.state.opened = true;
+            this.triggerEvent('selectorizer-before-open');
+            this.render();
+            this.triggerEvent('selectorizer-open');
+        }
+    }
+
+    close() {
+        if (this.state.opened) {
+            this.state.opened = false;
+            this.triggerEvent('selectorizer-before-close');
+            this.render();
+            this.triggerEvent('selectorizer-close');
+        }
+    }
+
+    render() {
+        const state = this.state;
+        const { selectorizer, list, text } = this.elements;
+        const select = this.select;
+
+        select.value = state.currentValue;
+        const currentIndex = select.selectedIndex || 0;
+        const option = selectorizer.querySelectorAll(`.${pluginName}__item`)[currentIndex];
+
+        if (option) {
+            text.innerText = option.innerText;
+            getSiblings(option).forEach(sibling => {
+                sibling.classList.remove('active');
+            });
+            option.classList.add('active');
+        } else {
+            text.innerText = '';
+        }
+
+        if (state.opened) {
+            selectorizer.classList.add('opened');
+        } else {
+            selectorizer.classList.remove('opened');
+        }
+
+        if (state.position === 'top') {
+            list.style.top = '100%';
+            list.style.bottom = 'auto';
+        } else {
+            list.style.top = 'auto';
+            list.style.bottom = '100%';
+        }
+    }
+
+    triggerEvent(eventName, ...args) {
+        if (this.events.hasOwnProperty(eventName)) {
+            this.events[eventName](this.select, this.state.currentValue, ...args);
+        }
+    }
+
+    on(eventName, cb) {
+        if (!this.events.hasOwnProperty(eventName)) {
+            this.events[eventName] = cb;
+        }
+    }
+
+    generateList() {
+        const list = createElement('ul', `${pluginName}__list`, '');
+        const currentOptions = this.select.querySelectorAll('option');
+        currentOptions.forEach((option) => {
+            const value = option.value;
+            const text = option.text;
+            const li = createElement('li', `${pluginName}__item`, text);
+            li.addEventListener('click', (e) => {
+                e.stopPropagation();
+                this.updateValue(value);
+                this.close();
+            });
+            list.appendChild(li);
+        });
+        return list;
+    }
+}
+
+class Data {
+    constructor() {
+        this.selects = [];
+    }
+
+    add(item) {
+        this.selects.push(item)
+    }
+
+    trigger(methodName, ...args) {
+        this.selects.forEach(e => {
+            e[methodName](...args);
+        })
+    }
+
+    on(...args) {
+        this.trigger('on', ...args);
+    }
+}
+
+const selectorize = (element, params) => {
+    let items;
+    if (!(element instanceof NodeList)) {
+        items = [element];
+    } else {
+        items = element;
+    }
+
+    const map = new Data();
+
+    items.forEach(select => {
+        const data = select[pluginName];
+        if (!data) {
+            select[pluginName] = new Selectorizer(select, params);
+            map.add(select[pluginName]);
+        } else {
+            map.add(data);
+        }
     });
 
-    document.body.addEventListener('click', (e) => {
-        if (!element.contains(e.target)) {
-            state.active = false;
-            render(element, list, text, state);
-        }
-    });
-
-    select.after(element);
-    element.prepend(select);
-
-    select.addEventListener('change', (e) => {
-        const index = e.target.selectedIndex;
-        state.currentOptionIndex = index;
-        state.currentText = state.items[index];
-        render(element, list, text, state);
-    })
+    return map;
 };
 
-// const wrap = (element, options) => {
-//     const state = {
-//         active: false,
-//         currentOptionIndex: 0,
-//         currentText: '',
-//         items: [],
-//         callbacks: options.callbacks
-//     };
-//
-//     const select = createElement('div', 'select', '');
-//
-//     const inner = createElement('div', 'select__inner', '');
-//     select.appendChild(inner);
-//
-//     const icon = createElement('div', 'select__icon', '');
-//     inner.appendChild(icon);
-//
-//     const list = createElement('ul', 'select__list', '');
-//     list.style.display = 'none';
-//
-//     const currentOptions = element.querySelectorAll('option');
-//
-//     const text = createElement('div', 'select__text', currentOptions[0].innerText);
-//     state.currentText = currentOptions[0].innerText;
-//     inner.appendChild(text);
-//
-//     currentOptions.forEach((option, idx) => {
-//         const optionText = option.text;
-//         state.items.push({value: option.value, text: optionText});
-//         const li = createElement('li', 'select__item', optionText);
-//         li.addEventListener('click', (e) => {
-//             e.stopPropagation();
-//             state.active = false;
-//             state.currentOptionIndex = idx;
-//             state.currentText = state.items[idx].text;
-//             if (state.callbacks.onchange) {
-//                 options.callbacks.onchange(select, state.items[idx].value);
-//             }
-//             render(select, list, text, state);
-//         });
-//
-//         list.appendChild(li);
-//     });
-//
-//     select.appendChild(list);
-//
-//     select.addEventListener('click', (e) => {
-//         if (window.innerWidth < 768) return;
-//         const position = window.pageYOffset;
-//         const h = window.innerHeight;
-//         const elY = select.offsetTop;
-//         const calc = (position + h) / 2;
-//         if (calc < elY) {
-//             state.position = 'bottom';
-//         } else {
-//             state.position = 'top';
-//         }
-//         state.active = !state.active;
-//         render(select, list, text, state);
-//     });
-//
-//     document.body.addEventListener('click', (e) => {
-//         if (!select.contains(e.target)) {
-//             state.active = false;
-//             render(select, list, text, state);
-//         }
-//     });
-//
-//     element.after(select);
-//     select.prepend(element);
-//
-//     element.addEventListener('change', (e) => {
-//         const index = e.target.selectedIndex;
-//         state.currentOptionIndex = index;
-//         state.currentText = state.items[index];
-//         render(select, list, text, state);
-//     })
-// };
-
-// export default selectorize;
+export default selectorize;
