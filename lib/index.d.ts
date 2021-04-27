f9653f60bc5c289fd50aea00751b3c71cfce293a
Denis Chertenko <denis.chertenko@mail.ru>
2021-04-27 22:55:34 +0300

    fix(lib): remove lib folder

--- lib/index.d.ts
+++
@@ -1,4 +0,0 @@
-import "@babel/polyfill";
-import { IOptions } from "./scripts/types";
-import { Controller } from "./scripts/Controller";
-export declare const selectorize: ($selectElement: HTMLSelectElement | NodeListOf<HTMLSelectElement>, options?: IOptions | undefined) => Controller;
